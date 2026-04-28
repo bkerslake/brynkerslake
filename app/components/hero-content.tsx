bad3dbc56e4e1a83ce33784311d92b525d99ed4d
bryn kerslake <brynkerslake@gmail.com>
2026-04-28 10:35:21 -0400

    remove illicit seals

--- app/components/hero-content.tsx
+++ app/components/hero-content.tsx
@@ -1,4 +1,3 @@
-import { InteractiveSeal } from "./interactive-seal";
 import { TeleprompterText } from "./teleprompter-text";
 
 type LinkRecord = {
@@ -19,23 +18,6 @@ export function HeroContent({ links, paragraphs }: HeroContentProps) {
 
         <div className="hero-body">
           <TeleprompterText paragraphs={paragraphs} />
-
-          <div className="seal-cluster" aria-hidden="true">
-            <InteractiveSeal
-              alt="Stylized U.S. State Department eagle"
-              className="seal-chip-eagle"
-              height={52}
-              src="/marks/state-eagle.svg"
-              width={52}
-            />
-            <InteractiveSeal
-              alt="Stylized Welsh dragon"
-              className="seal-chip-dragon"
-              height={64}
-              src="/marks/welsh-dragon.svg"
-              width={64}
-            />
-          </div>
 
           <div className="link-row">
             {links.map((link) => (
